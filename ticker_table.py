6dfd9d78dff6342e344e3fc3ca3a85cd9e247e28
Henry <henryxu@berkeley.edu>
2017-01-01 01:50:23 -0800

    Added sqlite3 support to ticker lookup.
    Happy New Year 2017!

--- ticker_table.py
+++ ticker_table.py
@@ -1,8 +1,11 @@
-import urllib.request, csv, re
+# -*- coding: utf-8 -*-
 
-def company_table(se):
+import urllib.request, csv, re, sqlite3
+
+def company_table(se = 'nasdaq', sql = False):
     """Returns table of company data from stock exchange 'se' 
     where each row contains the ticker symbol, lowercase company name, and NASDAQ website"""
+    assert se in ['nasdaq','nyse'], 'Not a valid stock exchange'
     try:
         with urllib.request.urlopen('http://www.nasdaq.com/screening/company-list.aspx') as response:
             html = response.read()
@@ -16,14 +19,27 @@ def company_table(se):
         with urllib.request.urlopen(url) as response:
             tickers = csv.reader(response.read().decode('utf-8').splitlines())
     except:
-        with open('data/companylist.csv', newline = '') as csvfile:
+        with open('data/companylist' + se + '.csv', newline = '') as csvfile:
             tickers = csv.reader(csvfile)
+            
     tickertable = []
     for row in tickers:           
-            tickertable.append(row)
-    return tickertable[1:]
-        
+        tickertable.append(row)
 
+    if sql:
+        conn = sqlite3.connect('data/companysql.sql')
+        curs = conn.cursor()
+        curs.executescript("DROP TABLE IF EXISTS companysql; CREATE TABLE companysql (symbol, name, sector);")
+        for row in tickertable:
+            to_db = [str(row[x]) for x in [0,1,6]]
+            curs.execute("INSERT INTO companysql (symbol, name, sector) VALUES (?, ?, ?);", to_db)
+            conn.commit()
+        conn.close()
+        
+    return tickertable[1:]
+
+    
+    
         
 
 
